55fa234d7a24f32812b1069406c4a329104b3b04
Tijana Topalovic <tijanatopalovic2002@gmail.com>
2025-09-21 15:54:09 +0200

    privremeni commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"; 
+import NavBar from "./navBar";
+import Home from "./home";
+
+function App() {
+   return ( 
+      <div className="App"> 
+        <NavBar /> 
+        <Home  /> 
+      </div>
+
+   ); 
+  } 
+  export default App;

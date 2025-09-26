429a2c9a23cc3e47dec0915ced1ec234b8e1e492
Tijana Topalovic <tijanatopalovic2002@gmail.com>
2025-09-26 18:37:08 +0200

    Homepage

---
+++ frontend/src/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div class="cyberpunk-frame">
+        <h1 class="glitch">
+  <span aria-hidden="true">GDE SAD</span>
+  GDE SAD
+  <span aria-hidden="true">GDE SAD</span>
+</h1>
+    <h1 class="naslov">Sve što se dešava – dešava se ovde.</h1>
+    <button data-text="KRENI U POTRAGU" class="submit-btn">
+  <span class="btn-text">KRENI U POTRAGU</span>
+</button>
+
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react' 
+
+function NavBar() {
+     return ( 
+        <div className="navBar" > 
+            <ul className="nav-links">
+        <li><a href="/">Događaji</a></li>
+        <li><a href="/">Kontakt</a></li>
+        <li><a href="/">Login</a></li>
+        <li><a href="/">Registracija</a></li>
+      </ul> 
+        </div> 
+        ) 
+    } 
+    export default NavBar
